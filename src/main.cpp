46dc5761fd4455ccb7d2d2cac4a477e926db3900
iwas108 <aditya@narin.co.id>
2021-05-03 14:44:38 +0800

    Ditambahkan fungsi getAmbientTemperature

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,36 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 const char* WIFI_SSID = "Wifi_Undiknas 3D";
 const char* WIFI_PASS = "";
 const char* HOSTNAME = "IOTPAGIESP32";
+
+OneWire oneWire(4);
+DallasTemperature sensors(&oneWire);
+
+float getAmbientTemperature()
+{
+  Serial.print("Requesting temperatures...");
+  sensors.requestTemperatures();
+  Serial.println("DONE");
+
+  float tempC = sensors.getTempCByIndex(0);
+
+  if(tempC != DEVICE_DISCONNECTED_C) 
+  {
+    Serial.print("Temperature for the device 1 (index 0) is: ");
+    Serial.println(tempC);
+    return tempC;
+  } 
+  else
+  {
+    Serial.println("Error: Could not read temperature data");
+    return -127;
+  }
+}
+
 
 void setup() {
   Serial.begin(115200);
@@ -21,8 +48,12 @@ void setup() {
   Serial.println("");
   Serial.println("WiFi connected successfully.");
 
+  sensors.begin();
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  getAmbientTemperature();
+  delay(3000);
 }
